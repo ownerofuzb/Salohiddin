8a90bd09846cef2f4a8b07c6d690e0361483cba9
ownerofuzb <147534794+ownerofuzb@users.noreply.github.com>
2023-10-10 20:58:49 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function restaran() {
+    document.getElementById("demo").style.display = "block"
+    document.getElementById("exitDemo").style.display = "block"
+    document.getElementById("exitDemo").style.zIndex = "999"
+}
+function exi() {
+    document.getElementById("demo").style.display = "none"
+    document.getElementById("exitDemo").style.display = "none"
+
+}
